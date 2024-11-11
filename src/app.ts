6a477a1bf77198816530c21244693b9e8b0a3483
Átila de Freitas <atila.jcdefreitas@protonmail.com>
2024-11-11 20:54:02 -0300

    minor changes

--- src/app.ts
+++ src/app.ts
@@ -26,7 +26,7 @@ import { SchedulerService } from "./services/scheduler.service";
 import swaggerJSDoc from "swagger-jsdoc";
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 app.use(
 	helmet({
